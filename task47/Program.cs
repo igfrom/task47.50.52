d3260aa215bcf17040359ae9a0c65133d1f5dee3
Igor From <ig.from@yandex.ru>
2022-12-04 21:08:42 +0700

    add tasks 47.50.52

---
+++ task47/Program.cs
@@ -0,0 +1,29 @@
+﻿// **Задача 47**. Задайте двумерный массив размером m×n, заполненный случайными 
+// вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Введите размерность массива m x n: ");
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите m: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = new Random().NextDouble();
+        Console.Write(matrix[i, j] + " ");
+    }    
+    Console.WriteLine();
+}
+
---
+++ task52/Program.cs
@@ -0,0 +1,52 @@
+﻿// **Задача 52.** Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите число строк массива");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[m, n];
+
+FillArray(numbers);
+PrintArray(numbers);
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    int summean = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        summean = summean + numbers[i, j];
+    }
+    Console.WriteLine(summean / m);
+}
+
+// формирование массива
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+// распечатывание массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
